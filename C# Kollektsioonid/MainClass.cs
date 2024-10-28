e4d775dba840432513bb5cf5a49cd2f7a680b772
MartinKemppi <dusperin1234@gmail.com>
2024-10-28 14:17:12 +0200

    28.10.24

--- C# Kollektsioonid/MainClass.cs
+++ C# Kollektsioonid/MainClass.cs
@@ -10,6 +10,8 @@ namespace C__Kollektsioonid
     {
         public static void Main()
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
             Console.WriteLine("ülesanne 1");
             Console.WriteLine("Sissesta numbrid (kasuta space)");
             int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
@@ -33,6 +35,47 @@ namespace C__Kollektsioonid
             }
             Console.WriteLine();
             Ulesane_2.ul2();
+
+
+
+            Ulesanne_4 estonia = new Ulesanne_4();
+
+            while (true)
+            {
+                Console.WriteLine("\nValige toiming:");
+                Console.WriteLine("1. Näidata pealinna maakonna nime järgi");
+                Console.WriteLine("2. Näita maakonda pealinna nime järgi");
+                Console.WriteLine("3. Lisa uus maakond ja pealinn");
+                Console.WriteLine("4. Testida teadmisi maakondade ja pealinnade kaupa");
+                Console.WriteLine("5. Väljapääs");
+                Console.Write("Teie valik: ");
+
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Sisesta maakonna nimi: ");
+                        string county = Console.ReadLine();
+                        estonia.ShowCapital(county);
+                        break;
+                    case "2":
+                        Console.Write("Sisesta pealinna nimi: ");
+                        string capital = Console.ReadLine();
+                        estonia.ShowCounty(capital);
+                        break;
+                    case "3":
+                        estonia.AddCounty();
+                        break;
+                    case "4":
+                        estonia.TestKnowledge();
+                        break;
+                    case "5":
+                        return;
+                    default:
+                        Console.WriteLine("Vale valik, proovi uuesti.");
+                        break;
+                }
+            }
         }
     }
 }
--- C# Kollektsioonid/Ulesanne 4.cs
+++ C# Kollektsioonid/Ulesanne 4.cs
@@ -6,7 +6,77 @@ using System.Threading.Tasks;
 
 namespace C__Kollektsioonid
 {
-    internal class Ulesanne_4
+    public class Ulesanne_4
     {
+
+        private Dictionary<string, string> counties = new Dictionary<string, string>()
+    {
+        { "Harju", "Tallinn" },
+        { "Tartu", "Tartu" },
+        { "Ida-Viru", "Jõhvi" },
+        { "Pärnu", "Pärnu" },
+        { "Viljandi", "Viljandi" }
+    };
+
+        public void ShowCapital(string county)
+        {
+            if (counties.ContainsKey(county))
+            {
+                Console.WriteLine($"Maakonna pealinn {county}: {counties[county]}");
+            }
+            else
+            {
+                Console.WriteLine($"Maakond {county} ei ole sõnaraamatust leitud.");
+                AddCounty(county);
+            }
+        }
+
+        public void ShowCounty(string capital)
+        {
+            string county = counties.FirstOrDefault(c => c.Value == capital).Key;
+            if (county != null)
+            {
+                Console.WriteLine($"Pealinna maakond {capital}: {county}");
+            }
+            else
+            {
+                Console.WriteLine($"Pealinn {capital} ei ole sõnaraamatust leitud.");
+                AddCounty(countyName: null, capital: capital);
+            }
+        }
+
+        public void AddCounty(string countyName = null, string capital = null)
+        {
+
+            Console.Write("Sisesta maakonna nimi: ");
+            countyName = Console.ReadLine();
+            Console.Write("Sisesta pealinna nimi: ");
+            capital = Console.ReadLine();
+        }
+
+        public void TestKnowledge()
+        {
+            Random random = new Random();
+            int correctAnswers = 0;
+
+            Console.WriteLine("Maakondade ja pealinnade tundmise test. Sisesta pealinna maakonnale või maakonnale pealinn:");
+
+            foreach (var item in counties.OrderBy(x => random.Next()))
+            {
+                Console.Write($"Sisesta {item.Key} pealinn: ");
+                string answer = Console.ReadLine();
+
+                if (answer.Equals(item.Value, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Õigus!");
+                    correctAnswers++;
+                }
+                else
+                {
+                    Console.WriteLine($"Valesti. Õige vastus: {item.Value}");
+                }
+            }
+            Console.WriteLine($"Teie tulemus: {correctAnswers} väljast {counties.Count}");
+        }
     }
 }
