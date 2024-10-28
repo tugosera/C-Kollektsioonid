e639dfdcbb4f01bb68b9b8254e54637b58fb1cf3
MartinKemppi <dusperin1234@gmail.com>
2024-10-28 13:06:02 +0200

    Add project files.

---
+++ C# Kollektsioonid/MainClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Kollektsioonid
+{
+    public class MainClass
+    {
+        public static void Main()
+        {
+            Console.WriteLine("ülesanne 1");
+            Console.WriteLine("Sissesta numbrid (kasuta space)");
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            if (numbers.Length == 1)
+            {
+                Console.WriteLine(numbers[0]);
+            }
+            else
+            {
+                int[] result = new int[numbers.Length];
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    int left = numbers[(i - 1 + numbers.Length) % numbers.Length];
+                    int right = numbers[(i + 1) % numbers.Length];
+                    result[i] = left + right;
+                }
+
+               Console.WriteLine(string.Join(" ", result));
+            }
+            Console.WriteLine();
+            Ulesane_2.ul2();
+        }
+    }
+}
---
+++ C# Kollektsioonid/Ulesane 2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Kollektsioonid
+{
+    public class Ulesane_2
+    {
+        public static void ul2()
+        {
+            Console.WriteLine("Ülesanne 2");
+
+            Random rnd = new Random();  
+            
+            List<int> numbers = new List<int> { };
+
+            for (int i = 0; i < 20; i++)
+            {
+                int a = rnd.Next(1,200);
+                numbers.Add(a);
+            }
+
+            List<int> sortNumbers = numbers.Where(n => n % 2 == 0).ToList();
+            sortNumbers.AddRange(numbers.Where(n => n % 2 != 0));
+
+
+            Console.WriteLine("Tavaline nimekiri: ");
+            Console.WriteLine(string.Join(" ", numbers));
+            Console.WriteLine("Sorteeritud nimekiri: ");
+            Console.WriteLine(string.Join(" ", sortNumbers));
+        }
+    }
+}
